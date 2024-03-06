115d7b88a8971c910773a3bcdc47ebad8d97c026
FacuTombesi <facundomtombesi@gmail.com>
2024-03-06 15:02:12 -0300

    added local storage hook

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
 export const currencyFormatter = new Intl.NumberFormat(undefined, {
   currency: "usd",
   style: "currency",
   minimumFractionDigits: 0
-});
+})
+
+export default function useLocalStorage(key, defaultValue) {
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(key)
+    if (jsonValue != null) return JSON.parse(jsonValue)
+    if (typeof defaultValue === "function") {
+      return defaultValue()
+    } else {
+      return defaultValue
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+
+  return [value, setValue]
+}
