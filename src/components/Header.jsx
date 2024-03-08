73f3adf2dc6489687bf087112acdc0f0b6eb2d04
FacuTombesi <facundomtombesi@gmail.com>
2024-03-08 13:53:48 -0300

    added header img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import { Button, Stack } from "react-bootstrap";
 
 export default function Header({ onAddBudgetClick, onAddExpenseClick }) {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768)
+    }
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
   return (
     <header>
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budgets</h1>
+      <Stack direction={isMobile ? "vertical" : "horizontal"} gap="2" className="mb-4">
+        <img src="./img/budgettracker_project-thumbnails.svg" alt="Budget Tracker" height="80px" className={isMobile ? "" : "me-auto"} />
         <Button variant="primary" onClick={onAddBudgetClick}>Add Budget</Button>
         <Button variant="outline-primary" onClick={onAddExpenseClick}>Add Expense</Button>
       </Stack>
