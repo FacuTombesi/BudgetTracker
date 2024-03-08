da6020495b1e1a3dd042c9d81364d37d3f2cc4be
FacuTombesi <facundomtombesi@gmail.com>
2024-03-08 17:04:43 -0300

    added button and icon when there are no budgets

--- src/App.js
+++ src/App.js
@@ -35,23 +35,38 @@ function App() {
           onAddBudgetClick={() => setShowAddBudgetModal(true)}
           onAddExpenseClick={openAddExpenseModal}
         />
-        <div style={{ display:"grid", gridTemplateColumns:"repeat(auto-fill, minmax(300px, 1fr))", gap:"1rem", alignItems:"flex-start" }}>
-          {budgets.map(budget => {
-            const currentAmount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0)
-            return (
-              <BudgetCard
-                key={budget.id}
-                creationDate={creationDate}
-                name={budget.name}
-                currentAmount={currentAmount}
-                maxAmount={budget.max}
-                onAddExpenseClick={() => openAddExpenseModal(budget.id)}
-                onViewExpenseClick={() => setViewExpensesModalBudgetId(budget.id)}
-              />
-            )
-          })}
-          <UncategorizedBudgetCard onAddExpenseClick={openAddExpenseModal} onViewExpenseClick={() => setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)} />
-        </div>
+        {(budgets.length !== 0) ? (
+          <div style={{ display:"grid", gridTemplateColumns:"repeat(auto-fill, minmax(300px, 1fr))", gap:"1rem", alignItems:"flex-start" }}>
+            {budgets.map(budget => {
+              const currentAmount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0)
+              return (
+                <BudgetCard
+                  key={budget.id}
+                  creationDate={creationDate}
+                  name={budget.name}
+                  currentAmount={currentAmount}
+                  maxAmount={budget.max}
+                  onAddExpenseClick={() => openAddExpenseModal(budget.id)}
+                  onViewExpenseClick={() => setViewExpensesModalBudgetId(budget.id)}
+                />
+              )
+            })}
+            <UncategorizedBudgetCard onAddExpenseClick={openAddExpenseModal} onViewExpenseClick={() => setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)} />
+          </div>
+        ) : (
+          <div style={{ display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center" }}>
+            <img
+              src="./img/add_icon.svg"
+              alt="Add"
+              height="60px"
+              onClick={() => setShowAddBudgetModal(true)}
+              style={{ cursor:"pointer" }}
+              onMouseOver={(e) => {e.currentTarget.src = "./img/add_icon_hover.svg"}}
+              onMouseOut={(e) => {e.currentTarget.src = "./img/add_icon.svg"}}
+            />
+            <span className="mt-2 fs-6" style={{ color:"#ADADAD" }}>Add a budget to start tracking</span>
+          </div>
+        )}
         <Container style={{ borderTop: "1px solid #DEE2E6", marginTop: "2rem", paddingTop: "2rem" }}>
           <TotalBudgetCard />
         </Container>
